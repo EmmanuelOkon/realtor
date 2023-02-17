4f472f9cee75d15b0f0532bc8f74c9d5125ddead
Emmanuel Okon <emmaudo27@gmail.com>
2023-02-17 14:23:41 +0100

    forgot password functionality enabled

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -3,26 +3,38 @@ import Locked from "../assets/lock.png";
 
 import { Link } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import { toast } from "react-toastify";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");
 
-  
   function onChange(e) {
-    setEmail(e.target.value)
+    setEmail(e.target.value);
   }
 
-  
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Reset link sent");
+    } catch (error) {
+      toast.error("Could not send reset link");
+    }
+  }
 
   return (
     <section>
-      <h1 className="text-xl md:text-3xl text-center mt-6 font-bold text-red-600 uppercase md:tracking-widest">Forgot Password</h1>
+      <h1 className="text-xl md:text-3xl text-center mt-6 font-bold text-red-600 uppercase md:tracking-widest">
+        Forgot Password
+      </h1>
       <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto ">
         <div className="w-full md:w-[50%] lg:w-[50%] ">
           <img className="w-full" src={Locked} alt="locker" />
         </div>
         <div className="w-full md:w-[50%] bg-rd-700 py-6  lg:px-6 ">
-          <form className="w-full">
+          <form className="w-full" onSubmit={onSubmit}>
             <input
               className="w-full px-4 py-2 md:text-lg text-gray-700 bg-white border-[2px] border-grey-300 rounded-md outline-none ring-0 focus:ring-0 focus:outline-none transition ease-in-out mb-6"
               type="email"
@@ -31,7 +43,7 @@ export default function ForgotPassword() {
               onChange={onChange}
               placeholder="Enter e-mail address"
             />
-            
+
             <div className="mb-4 flex justify-between whitespace-nowrap text-sm lg:text-base font-normal md:font-medium">
               {/* <p className="mb-6">
                 Don't have an account?
@@ -43,12 +55,12 @@ export default function ForgotPassword() {
                 </Link>
               </p> */}
               <p>
-              Remembered password? 
+                Remembered password?
                 <Link
                   to="/signin"
                   className="ml-2 text-sm lg:text-base text-green-600 hover:text-green-800 transition duration-200 ease-in-out"
                 >
-                 Sign in instead 
+                  Sign in instead
                 </Link>
               </p>
             </div>
